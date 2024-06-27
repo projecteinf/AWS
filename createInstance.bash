f1e8651c5eff511422562549bdab5e6e91549f74
Miquel Boada <projecteinf@boscdelacoma.cat>
2024-06-27 09:35:30 +0200

    Obtenir Security Group Id

--- createInstance.bash
+++ createInstance.bash
@@ -1,7 +1,9 @@
 #/bin/bash
 TOTALGROUPS=`aws ec2 describe-security-groups --query "SecurityGroups[*].[GroupId,GroupName,Description]" --output table | grep default | grep sg- | wc -l`
+SECURITYGROUPID=`aws ec2 describe-security-groups --query "SecurityGroups[*].[GroupId,GroupName,Description]" --output table | grep default | grep sg- | awk '{print $2}'`
 if [ $TOTALGROUPS -eq 1 ]; then
     echo "There is $TOTALGROUPS security group"
 else
     echo "There are $TOTALGROUPS security groups"
 fi
+echo "Security Group ID: $SECURITYGROUPID"
