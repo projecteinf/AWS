5227b98d75cc72eb5cca9367de6f6f650214f863
Miquel Boada <projecteinf@boscdelacoma.cat>
2024-06-27 09:42:04 +0200

    Obtenir l'id de totes les imatges. No funcional!

--- createInstance.bash
+++ createInstance.bash
@@ -1,6 +1,10 @@
 #/bin/bash
 TOTALGROUPS=`aws ec2 describe-security-groups --query "SecurityGroups[*].[GroupId,GroupName,Description]" --output table | grep default | grep sg- | wc -l`
 SECURITYGROUPID=`aws ec2 describe-security-groups --query "SecurityGroups[*].[GroupId,GroupName,Description]" --output table | grep default | grep sg- | awk '{print $2}'`
+AMIID=`aws ec2 describe-images --owners self --query 'Images[*].[ImageId,Name]' --output table | grep "Ubuntu" | awk '{print $2}'`
+
+echo "AMI ID: $AMIID"
+
 if [ $TOTALGROUPS -eq 1 ]; then
     echo "There is $TOTALGROUPS security group"
 else
