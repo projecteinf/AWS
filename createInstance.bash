4ebef0325adcfa7225d7d2d6c646b72428de60ce
Miquel Boada <projecteinf@boscdelacoma.cat>
2024-06-27 09:44:27 +0200

    Obtenir l'id de totes les imatges free tier. Cal filtrar molt més!

--- createInstance.bash
+++ createInstance.bash
@@ -1,7 +1,7 @@
 #/bin/bash
 TOTALGROUPS=`aws ec2 describe-security-groups --query "SecurityGroups[*].[GroupId,GroupName,Description]" --output table | grep default | grep sg- | wc -l`
 SECURITYGROUPID=`aws ec2 describe-security-groups --query "SecurityGroups[*].[GroupId,GroupName,Description]" --output table | grep default | grep sg- | awk '{print $2}'`
-AMIID=`aws ec2 describe-images --owners self --query 'Images[*].[ImageId,Name]' --output table | grep "Ubuntu" | awk '{print $2}'`
+AMIID=`aws ec2 describe-images --owners self amazon --query 'Images[*].[ImageId,Name]' --output table | awk '{print $2}'`
 
 echo "AMI ID: $AMIID"
 
