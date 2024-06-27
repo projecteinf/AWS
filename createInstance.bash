5ac55849ca4b00b3377acd72c6b636470dac3e55
Miquel Boada <projecteinf@boscdelacoma.cat>
2024-06-27 09:51:22 +0200

    Filtrar per màquines disponibles

--- createInstance.bash
+++ createInstance.bash
@@ -1,7 +1,11 @@
 #/bin/bash
 TOTALGROUPS=`aws ec2 describe-security-groups --query "SecurityGroups[*].[GroupId,GroupName,Description]" --output table | grep default | grep sg- | wc -l`
 SECURITYGROUPID=`aws ec2 describe-security-groups --query "SecurityGroups[*].[GroupId,GroupName,Description]" --output table | grep default | grep sg- | awk '{print $2}'`
-AMIID=`aws ec2 describe-images --owners self amazon --query 'Images[*].[ImageId,Name]' --output table | awk '{print $2}'`
+
+# owners self amazon : màquines free tier
+AMIID=`aws ec2 describe-images --owners self amazon --filters "Name=state,Values=available" --query 'Images[*].[ImageId,Name]' --output table | awk '{print $2}'`
+
+
 
 echo "AMI ID: $AMIID"
 
