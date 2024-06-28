0d09a786ba51111c40d5e7220da53d6bbe71b4df
Miquel Boada <projecteinf@boscdelacoma.cat>
2024-06-28 09:24:29 +0200

    Referències bibliogràfiques

--- Crear Instància EC2.md
+++ Crear Instància EC2.md
@@ -2,10 +2,17 @@
 
 To create an ec2 instance using CLI, you need the following.
 
-Security group ID
-Key pair name
-AMI Id
-Subnet ID
+    Get VPC ID and Subnet ID
+    Get AMI Id
+    Create Security Group
+    Create SSH Key Pair
+    AWS CLI Command to Create ec2
+    AWS CLI Command to Create ec2 Instance With User Data
 
 # Security group ID
 
+# Webgrafia
+
+[https://medium.com/@corymaklin/tutorial-amazon-web-services-part-1-create-virtual-machines-with-aws-cli-b900702bf286](https://medium.com/@corymaklin/tutorial-amazon-web-services-part-1-create-virtual-machines-with-aws-cli-b900702bf286)
+
+[How to Use AWS CLI to Create an EC2 instance](https://devopscube.com/use-aws-cli-create-ec2-instance/)
--- createInstance.bash
+++ createInstance.bash
@@ -1,27 +1,15 @@
 #/bin/bash
 
-# NO HI HA UNA FORMA CLARA DE VEURE LES IMATGES QUE NO SÓN FREE-TIER
-# Obtenir la informació de les dades d'una imatge que no és free-trie
-
-NOFREETIER=`aws ec2 describe-images --filters "Name=image-id,Values=ami-0301be0087bfbe7c8" --owners amazon --query "Images[*]" --output table`
-
-FREETIER=`aws ec2 describe-images --filters "Name=image-id,Values=ami-01b799c439fd5516a" --owners amazon --query "Images[*]" --output table`
-
-echo "No Free Tier: $NOFREETIER"
-echo -e "\n\n**********************************************************************************************************************************\n\n"
-echo "Free Tier: $FREETIER"
-
-
-# ********************************************************************************************************************
-
+# Font: https://medium.com/@corymaklin/tutorial-amazon-web-services-part-1-create-virtual-machines-with-aws-cli-b900702bf286
 
 TOTALGROUPS=`aws ec2 describe-security-groups --query "SecurityGroups[*].[GroupId,GroupName,Description]" --output table | grep default | grep sg- | wc -l`
 SECURITYGROUPID=`aws ec2 describe-security-groups --query "SecurityGroups[*].[GroupId,GroupName,Description]" --output table | grep default | grep sg- | awk '{print $2}'`
+AMIID['AMAZON']=['ami-01b799c439fd5516a','ami-0195204d5dce06d99','ami-0b8aeb1889f1a812a']
 
-
-
-
-
+ 
+#   Free tier
+#       In your first year includes 750 hours of t2.micro (or t3.micro in the Regions in which t2.micro is unavailable) instance usage on free tier AMIs per month, 
+#       750 hours of public IPv4 address usage per month, 30 GiB of EBS storage, 2 million IOs, 1 GB of snapshots, and 100 GB of bandwidth to the internet.
 
 
 
@@ -38,3 +26,7 @@ else
     echo "There are $TOTALGROUPS security groups"
 fi
 echo "Security Group ID: $SECURITYGROUPID"
+
+# At minimum, we need to open port 22 (SSH protocol) so that you can connect to your instance.
+
+# aws ec2 authorize-security-group-ingress --group-name example --protocol tcp --port 22 --cidr 0.0.0.0/0
